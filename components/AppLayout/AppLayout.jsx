5d3a6943f8256fd7d5fd73332ad2678b19c860c3
Manuel <manuelborches@icloud.com>
2022-09-19 15:40:36 +0100

    feat: start project basic structure

---
+++ components/AppLayout/AppLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+import NavBar from "./NavBar";
+
+const AppLayout = ({ children, title = "Rick and Morty" }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="rick and morty app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavBar />
+
+      <main className={styles.main}>{children}</main>
+    </>
+  );
+};
+
+export default AppLayout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { store } from "../src/app/store";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import AppLayout from "../components/AppLayout/AppLayout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
